545119b588b79d5814383e0a87d5a8b1102ecc7d
ghufron akbar <alan.040219.gam@gmail.com>
2024-08-14 01:23:29 +0700

    add sql

--- pengaduan.sql
+++ pengaduan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 05, 2024 at 04:12 AM
+-- Generation Time: Aug 13, 2024 at 08:23 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -39,7 +39,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`adminId`, `nama`, `email`, `password`) VALUES
-(1, 'Hasan Rama', 'hasanrama@gmail.com', 'hasanrama11');
+(1, 'Hasan Rama', 'hasanrama@gmail.com', '25d55ad283aa400af464c76d713c07ad');
 
 -- --------------------------------------------------------
 
@@ -54,6 +54,8 @@ CREATE TABLE `pengaduan` (
   `telepon` varchar(255) NOT NULL,
   `dukuh` varchar(255) NOT NULL,
   `isi` text NOT NULL,
+  `foto` text DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT 1,
   `tanggal` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -61,13 +63,9 @@ CREATE TABLE `pengaduan` (
 -- Dumping data for table `pengaduan`
 --
 
-INSERT INTO `pengaduan` (`pengaduanId`, `nama`, `nik`, `telepon`, `dukuh`, `isi`, `tanggal`) VALUES
-(31, 'Gupong', '123', '123', '123', '123', '2024-08-05'),
-(32, 'Anonim', 'Anonim', 'Anonim', 'Anonim', '123', '2024-08-05'),
-(33, 'Anonim', 'Anonim', 'Anonim', 'Anonim', 'qwr', '2024-08-05'),
-(34, 'Anonim', 'Anonim', 'Anonim', 'Anonim', '', '2024-08-05'),
-(35, 'Anonim', 'Anonim', 'Anonim', 'Anonim', '', '2024-08-05'),
-(36, 'Anonim', 'Anonim', 'Anonim', 'Anonim', '123', '2024-08-05');
+INSERT INTO `pengaduan` (`pengaduanId`, `nama`, `nik`, `telepon`, `dukuh`, `isi`, `foto`, `status`, `tanggal`) VALUES
+(56, 'Anonim', 'Anonim', 'Anonim', 'Anonim', 'Sakit Perut', '66bba443271a04.10999611.png', 2, '2024-08-14'),
+(57, 'Anonim', 'Anonim', 'Anonim', 'Anonim', 'Sakit Kepala', NULL, 1, '2024-08-14');
 
 --
 -- Indexes for dumped tables
@@ -99,7 +97,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `pengaduan`
 --
 ALTER TABLE `pengaduan`
-  MODIFY `pengaduanId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `pengaduanId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
