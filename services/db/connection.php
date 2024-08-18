257630e2c3186ee169823e33a04c71152d7aaa25
ghufron akbar <alan.040219.gam@gmail.com>
2024-08-18 14:51:29 +0700

    htaccess

--- services/db/connection.php
+++ services/db/connection.php
@@ -7,5 +7,6 @@ $dbname = "pengaduan";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
+    echo '<script>console.log("Connection failed: " . $conn->connect_error);</script>';
     die("Connection failed: " . $conn->connect_error);
 }
